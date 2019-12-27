240a47067f701a122fa660ff29324162f8ea9be5
xxyxt <1033279615@qq.com>
2019-12-27 13:02:41 +0800

    MTS

--- solution.m
+++ solution.m
@@ -1,5 +1,6 @@
 clear;
 clc;
+tic;
 syms xi z;
 v=0.3;
 h=0.065;
@@ -12,13 +13,13 @@ B=[1;0;0;0];
 X=A\B;
 s(xi,z)=(X(1,1)+(1-2*v+xi*z/a)*X(2,1))*exp(-xi*z/a)-(X(3,1)-(1-2*v-xi*z/a)*X(4,1))*exp(xi*z/a);
 N=500;
-z_h=0.06;
+z_h=0.065;
 step_z=0.001;
 h=0.065;
 step_x=0.1;
 fun_array=zeros(N/step_x+1,z_h/step_z+1);
 index_y=1;
-for z=0.041:step_z:z_h
+for z=0:step_z:z_h
     index_x=1;
     for xi=0:step_x:N
         temp=s(xi,z);
@@ -27,8 +28,8 @@ for z=0.041:step_z:z_h
     end
     index_y=index_y+1;
 end
-path='C:\Users\tx\Desktop\20191111\data\int';
-fileName='fun_array_different_z.xlsx';
+path='D:\matlabworkbench\data';
+fileName='fun_array_different_z61_z80.xlsx';
 xlswrite([path fileName],fun_array);
-
+toc;
 
